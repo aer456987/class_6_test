5aaa6964f79db356bd90cc304c4c64466182a946
Hua Yen <aer456987@gmail.com>
2020-04-12 15:23:03 +0800

    修改變數名稱

--- dis_test.py
+++ dis_test.py
@@ -1,14 +1,14 @@
-dis = {
+dic = {
 	'tomato': '番茄',
 	'apple': '蘋果'
 }
 while True:
-	user_input = input("輸入要查詢的字(輸入q離開): ")
+	user_input = input('輸入要查詢的字(輸入q離開): ')
 	if user_input == 'q':
 		break
-	elif user_input in dis:
+	elif user_input in dic:
 		print(dis[user_input])
-	elif user_input not in dis:
+	elif user_input not in dic:
 		print('［找不到資料。］')
 		user_ins = input('r=重新查詢/n=新增資料: ')
 		if user_ins == 'r':
