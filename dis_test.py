63e8690a502e10126615a2786abf9555a9521889
Hua Yen <aer456987@gmail.com>
2020-04-17 09:51:21 +0800

    修改註記

--- dis_test.py
+++ dis_test.py
@@ -5,13 +5,14 @@ def dic_flie():
     }
     while True:
         print('指令: 顯示所有字典(all)；新增資料(n)；離開(q)')
-        user_input = input('輸入要查詢的字: ')
+        user_input = input('輸入[指令]或是[要查詢的字]: ')
         if user_input == 'all':    # 顯示所有資料
             print(dic)
         elif user_input == 'n':    # 新增資料
             new_key = input('輸入key: ')
             new_value = input('輸入value: ')
-            dic.setdefault(new_key, new_value)
+            # 將資料新增到字典裡 →也可以寫成 dic.setdefault(new_key, new_value)
+            dic[new_key] = new_value
             print('新增完成！')
         elif user_input == 'q':    # 離開
             break
@@ -19,7 +20,6 @@ def dic_flie():
             print(dic[user_input])
         elif user_input not in dic:
             print('［找不到資料。］')
-        
         print('==============')
     return dic
 dic_flie()
